df250bdf210d16b0a2efa89d67792882bbfc7f69
Travis Prosser <travisprosser@gmail.com>
2020-03-29 20:45:06 -0300

    add basic web page templates

--- fuel_logger/app/routes.py
+++ fuel_logger/app/routes.py
@@ -1,11 +1,26 @@
 from app import app 
 from flask import render_template
-from app.models import Fillup
+from app.models import Fillup, Vehicle
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template('home.html')
+
+
+@app.route("/vehicles/<vehicle_id>")
+def vehicle(vehicle_id):
+    v = Vehicle.query.get(vehicle_id)
+    if v is None:
+        return render_template('home.html')
+    return render_template('vehicle.html', vehicle=v)
+
+
+@app.route('/users/<username>')
+def user(username):
+    u = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=u)
+
 
 
 @app.route('/test')
