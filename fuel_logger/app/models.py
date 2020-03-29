de88987f6e39ef3f4d264e12db8ef3593a3230b6
Travis Prosser <travisprosser@gmail.com>
2020-03-29 17:28:57 -0300

    fix db init

--- fuel_logger/app/models.py
+++ fuel_logger/app/models.py
@@ -18,11 +18,21 @@ class Vehicle(db.Model):
     model = db.Column(db.String, nullable=False)
     year = db.Column(db.Integer)
 
+    fill_ups = db.relationship('Fillup', backref='vehicle', lazy='dynamic')
+
     def __repr__(self):
         return "<Vehicle {} {}>".format(self.make, self.model)
 
-class Entry(db.Model):
+class Fillup(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
-    vehicle_id = db.Column(db.Integer, db.ForeignKey(Vehicle.id))
+    vehicle_id = db.Column(db.Integer, db.ForeignKey(Vehicle.id), nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    odometer_km = db.Column(db.Integer, nullable=False)
+    fuel_amt_l = db.Column(db.Float, nullable=False)
+
+    @property
+    def owner(self):
+        return self.vehicle.owner
+
+    def __repr__(self):
+        return "<Fillup date={}, vehicle={}, fuel_L={}, dist_km={}>".format(self.timestamp, self.vehicle.model, self.fuel_amt_l, self.odometer_km)
--- fuel_logger/fuel_logger.py
+++ fuel_logger/fuel_logger.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.models import User, Vehicle, Entry
+from app.models import User, Vehicle, Fillup
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Vehicle': Vehicle, 'Entry':Entry}
+    return {'db': db, 'User': User, 'Vehicle': Vehicle, 'Fillup': Fillup}
