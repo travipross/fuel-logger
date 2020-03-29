64a344f278e3cf7d88579a9d59523e7d8e3afdf1
Travis Prosser <travisprosser@gmail.com>
2020-03-29 17:43:29 -0300

    add html templates

--- fuel_logger/app/routes.py
+++ fuel_logger/app/routes.py
@@ -1,6 +1,14 @@
 from app import app 
+from flask import render_template
+from app.models import Fillup
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello world"
+    return render_template('home.html')
+
+
+@app.route('/test')
+def test():
+    f = Fillup.query.first()
+    return render_template('_fillup.html', fillup=f)
