a55e09ef73ebe88f695ef83b2bccbb6104d4feae
Travis Prosser <travisprosser@gmail.com>
2020-03-29 21:55:00 -0300

    user logins and base template

--- fuel_logger/app/models.py
+++ fuel_logger/app/models.py
@@ -1,15 +1,25 @@
-from app import db
+from app import db, login
 from datetime import datetime
+from hashlib import md5 
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
     email = db.Column(db.String)
+    password_hash = db.Column(db.String(128))
 
     vehicles = db.relationship('Vehicle', backref='owner', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Vehicle(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -36,3 +46,9 @@ class Fillup(db.Model):
 
     def __repr__(self):
         return "<Fillup date={}, vehicle={}, fuel_L={}, dist_km={}>".format(self.timestamp, self.vehicle.model, self.fuel_amt_l, self.odometer_km)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
