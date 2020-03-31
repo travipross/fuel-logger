2028e8f0158d7b44d2fc4394e654dc6ff8d17dea
Travis Prosser <travisprosser@gmail.com>
2020-03-30 22:31:54 -0300

    add bootstrap and moment.js

--- fuel_logger/app/__init__.py
+++ fuel_logger/app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,5 +12,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+moment = Moment(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, errors
