fb0df041fb76ea8df17b4f4f242351dd9d4f1b5b
Travis Prosser <travisprosser@gmail.com>
2023-05-19 00:15:24 -0300

    test: Add initial unit test

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+import pandas as pd
+import os
+
+@pytest.fixture
+def corolla_df():
+    print(os.getcwd())
+    return pd.read_csv("tests/resources/fuel_logs_Corolla.csv", parse_dates=["timestamp"])
