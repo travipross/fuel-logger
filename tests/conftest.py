ce65237669f4cf415d26eaffd1b5ef5543ac2f94
Travis Prosser <travisprosser@gmail.com>
2023-05-19 09:57:45 -0300

    test: Add more fixtures and suppress deprecation warnings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 import os
-
+from fuel_logger.fuel_logger import create_app
 
 @pytest.fixture
 def corolla_df():
@@ -9,3 +9,15 @@ def corolla_df():
     return pd.read_csv(
         "tests/resources/fuel_logs_Corolla.csv", parse_dates=["timestamp"]
     )
+
+@pytest.fixture
+def app_fixture():
+     
+    os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
+    return create_app()
+
+@pytest.fixture
+def test_client(app_fixture):
+    with app_fixture.test_client() as client:
+        yield client
+
