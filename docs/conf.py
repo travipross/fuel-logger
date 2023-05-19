fc96926c84464dc4f2ef009687ab6d9ca8482cbe
Travis Prosser <travisprosser@gmail.com>
2023-05-19 12:47:04 -0300

    formatting

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Fuel Logger'
-copyright = '2023, travipross'
-author = 'travipross'
+project = "Fuel Logger"
+copyright = "2023, travipross"
+author = "travipross"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinxcontrib.napoleon'
-]
+extensions = ["sphinxcontrib.napoleon"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
