7c94288018732bddc539535ac4a6d36c07e83a1e
Travis Prosser <travisprosser@gmail.com>
2023-05-25 13:34:32 -0300

    test: Add integration test for deleting vehicle

--- tests/integration/api/test_api_vehicle.py
+++ tests/integration/api/test_api_vehicle.py
@@ -4,6 +4,25 @@ from fuel_logger import db
 import uuid
 
 
+@pytest.fixture
+def vehicle_id_to_delete(app_fixture):
+    with app_fixture.app_context():
+        random_model = f"TestModel-{uuid.uuid4()}"
+        vehicle = Vehicle(
+            make="TestMake",
+            model=random_model,
+            year=2000,
+        )
+        db.session.add(vehicle)
+        db.session.commit()
+        yield vehicle.id
+
+    with app_fixture.app_context():
+        if Vehicle.query.get(vehicle.id) is not None:
+            db.session.delete(vehicle)
+            db.session.commit()
+
+
 def test_vehicle_list(test_client, basic_auth_header):
     resp = test_client.get("/api/vehicles", headers=basic_auth_header)
     assert resp.status_code == 200
@@ -63,7 +82,7 @@ def test_vehicle_create(test_client, basic_auth_header, app_fixture):
         )
 
 
-def test_vehicle_create_empty(test_client, basic_auth_header, app_fixture):
+def test_vehicle_create_empty(test_client, basic_auth_header):
     # Make POST request to create new vehicle
     resp = test_client.post("/api/vehicles", headers=basic_auth_header, json={})
 
@@ -71,3 +90,24 @@ def test_vehicle_create_empty(test_client, basic_auth_header, app_fixture):
     assert resp.status_code == 400
     assert resp.json.get("error") == "Bad Request"
     assert "Missing data for required field" in resp.json.get("message")
+
+
+def test_vehicle_delete(
+    test_client, basic_auth_header, app_fixture, vehicle_id_to_delete
+):
+    # Ensure vehicle exists
+    with app_fixture.app_context():
+        assert Vehicle.query.get(vehicle_id_to_delete) is not None
+
+    # Make HTTP DELETE request
+    resp = test_client.delete(
+        f"/api/vehicles/{vehicle_id_to_delete}", headers=basic_auth_header
+    )
+
+    # Assert API responded correctly
+    assert resp.status_code == 200
+    assert resp.json.get("status") == "DELETED"
+
+    # Confirm vehicle no longer exists in database
+    with app_fixture.app_context():
+        assert Vehicle.query.get(vehicle_id_to_delete) is None
