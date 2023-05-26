0680cc3fed532ef2911c03047b659bc992b9bd3e
Travis Prosser <travisprosser@gmail.com>
2023-05-25 23:49:35 -0300

    test: More assertions for email test

--- tests/unit/utils/test_email.py
+++ tests/unit/utils/test_email.py
@@ -17,3 +17,13 @@ def test_send_email(app_fixture, mocker):
 
     time.sleep(0.05)
     mock_mailer.send.assert_called_once()
+    assert mock_mailer.send.call_args.args[0].subject == "Test"
+    assert (
+        mock_mailer.send.call_args.args[0].sender == "testapp.flask@flaskapp.test.com"
+    )
+    assert mock_mailer.send.call_args.args[0].recipients == ["travisprosser@gmail.com"]
+    assert mock_mailer.send.call_args.args[0].body == "Hello world, this is a test"
+    assert (
+        mock_mailer.send.call_args.args[0].html
+        == "<h1>Hello world, this is a test</h1>"
+    )
