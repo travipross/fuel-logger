8788fa326b709962483145c209c8c1c8dd859a10
Travis Prosser <travisprosser@gmail.com>
2023-05-26 21:21:14 -0300

    test: Update test to check new base template content

--- tests/integration/routes/test_index.py
+++ tests/integration/routes/test_index.py
@@ -1,6 +1,6 @@
 def test_index(test_client):
     resp = test_client.get("/index", follow_redirects=True)
-    assert b"Travis' Custom Fuel Logger" in resp.data
+    assert "Fuel Logger App" in resp.text
 
     resp = test_client.get("/", follow_redirects=True)
-    assert b"Travis' Custom Fuel Logger" in resp.data
+    assert "Fuel Logger App" in resp.text
