9de5fa998bb3a79f99b5090b20e6bec2d3861c90
Travis Prosser <travisprosser@gmail.com>
2023-05-30 15:17:01 -0300

    test: Add integration tests for vehicle creation

--- tests/integration/routes/test_vehicles.py
+++ tests/integration/routes/test_vehicles.py
@@ -84,3 +84,53 @@ def test_set_fav_vehicle__exists(
         with app_fixture.app_context():
             user = User.query.get(test_user_id)
             assert user.get_favourite_vehicle().id == secondary_vehicle_id
+
+
+def test_add_vehicle__unauthenticated(test_client):
+    resp = test_client.get(f"/add_vehicle", follow_redirects=True)
+    assert resp.status_code == 200
+    assert 302 in [r.status_code for r in resp.history]
+    assert f"Sign In" in resp.text
+
+
+def test_add_vehicle__get(app_fixture, test_user_id):
+    with app_fixture.app_context():
+        test_user = User.query.get(test_user_id)
+        with app_fixture.test_client(user=test_user) as test_client_authenticated:
+            resp = test_client_authenticated.get(
+                f"/add_vehicle",
+                follow_redirects=True,
+            )
+
+        assert resp.status_code == 200
+        assert "Add Vehicle" in resp.text
+
+
+def test_add_vehicle__post_complete(app_fixture, test_user_id, test_vehicle_id):
+    with app_fixture.app_context():
+        test_user = User.query.get(test_user_id)
+
+        assert test_user.vehicles.filter(Vehicle.id != test_vehicle_id).count() == 0
+
+        with app_fixture.test_client(user=test_user) as test_client_authenticated:
+            resp = test_client_authenticated.post(
+                "/add_vehicle",
+                follow_redirects=True,
+                data={
+                    "make": "FormTestMake",
+                    "model": "FormTestModel",
+                    "year": 2345,
+                    "odo_unit": "km",
+                },
+            )
+
+        assert resp.status_code == 200
+        assert f"Your vehicle has been added" in resp.text
+
+        assert test_user.vehicles.filter(Vehicle.id != test_vehicle_id).count() == 1
+        new_vehicle = test_user.vehicles.filter(Vehicle.id != test_vehicle_id).one()
+
+        assert new_vehicle.make == "FormTestMake"
+        assert new_vehicle.model == "FormTestModel"
+        assert new_vehicle.year == 2345
+        assert new_vehicle.odo_unit == "km"
