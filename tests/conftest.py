9e470c822896dd44cdde34a5e415e7dc4554dfb7
Travis Prosser <travisprosser@gmail.com>
2023-05-30 18:15:52 -0300

    test: Move database migration fixture to top level conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,12 @@ import pandas as pd
 from fuel_logger.fuel_logger import create_app
 from fuel_logger.config import TestingConfig
 from flask_login import FlaskLoginClient
+import flask_migrate
 
 
 @pytest.fixture
 def corolla_df():
-    return pd.read_csv(
+    yield pd.read_csv(
         "tests/resources/fuel_logs_Corolla.csv", parse_dates=["timestamp"]
     )
 
@@ -17,3 +18,14 @@ def app_fixture():
     app_fixture = create_app(config_class=TestingConfig)
     app_fixture.test_client_class = FlaskLoginClient
     yield app_fixture
+
+
+@pytest.fixture(autouse=True, scope="session")
+def setup_database(app_fixture):
+    with app_fixture.app_context():
+        flask_migrate.upgrade(directory="fuel_logger/migrations")
+
+    yield
+
+    with app_fixture.app_context():
+        flask_migrate.downgrade(directory="fuel_logger/migrations", revision="base")
