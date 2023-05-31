acad462e47f7421f0b49806ffe23908d0841ed09
Travis Prosser <travisprosser@gmail.com>
2023-05-31 09:05:54 -0300

    test: Add authenticated test for index page

--- tests/integration/routes/test_index.py
+++ tests/integration/routes/test_index.py
@@ -1,6 +1,21 @@
-def test_index(test_client):
+from fuel_logger.models import User
+
+
+def test_index__unauthenticated(test_client):
     resp = test_client.get("/index", follow_redirects=True)
-    assert "Fuel Logger App" in resp.text
+    assert "Sign In" in resp.text
 
     resp = test_client.get("/", follow_redirects=True)
-    assert "Fuel Logger App" in resp.text
+    assert "Sign In" in resp.text
+
+
+def test_index__authenticated(app_fixture, test_user_id):
+    with app_fixture.app_context():
+        test_user = User.query.get(test_user_id)
+
+        with app_fixture.test_client(user=test_user) as test_client_authenticated:
+            resp = test_client_authenticated.get("/index", follow_redirects=True)
+            assert "Home page" in resp.text
+
+            resp = test_client_authenticated.get("/", follow_redirects=True)
+            assert "Home page" in resp.text
