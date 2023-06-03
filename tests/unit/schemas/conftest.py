e3dd2a1b4c47669c7773052f8f506aae0b1f9086
Travis Prosser <travisprosser@gmail.com>
2023-06-03 12:05:56 -0300

    test: Add unit tests for fillup schema

--- tests/unit/schemas/conftest.py
+++ tests/unit/schemas/conftest.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 import pytest
 
 from fuel_logger import db
-from fuel_logger.models import User, Vehicle
+from fuel_logger.models import Fillup, User, Vehicle
 
 
 @pytest.fixture
@@ -105,4 +107,62 @@ def sample_vehicle_2__serialized(sample_vehicle_id_2):
         "year": 2002,
         "odo_unit": "mi",
         "id": sample_vehicle_id_2,
+    }
+
+
+@pytest.fixture
+def sample_fillup_id_1(app_fixture, sample_vehicle_id_1):
+    with app_fixture.app_context():
+        fillup = Fillup(
+            vehicle_id=sample_vehicle_id_1,
+            timestamp=datetime(year=2020, month=1, day=1, hour=12, minute=0, second=0),
+            odometer_km=500,
+            fuel_amt_l=25,
+        )
+        db.session.add(fillup)
+        db.session.commit()
+        yield fillup.id
+        db.session.delete(fillup)
+        db.session.commit()
+
+
+@pytest.fixture
+def sample_fillup_1__serialized(sample_fillup_id_1, sample_vehicle_id_1):
+    yield {
+        "id": sample_fillup_id_1,
+        "timestamp": "2020-01-01T12:00:00",
+        "odometer_km": 500,
+        "fuel_amt_l": 25,
+        "vehicle_id": sample_vehicle_id_1,
+        "vehicle": "2001 test-make1 test-model1",
+    }
+
+
+@pytest.fixture
+def sample_fillup_id_2(app_fixture, sample_vehicle_id_1):
+    with app_fixture.app_context():
+        fillup = Fillup(
+            vehicle_id=sample_vehicle_id_1,
+            timestamp=datetime(year=2020, month=1, day=2, hour=12, minute=0, second=0),
+            odometer_km=1000,
+            fuel_amt_l=35,
+        )
+        db.session.add(fillup)
+        db.session.commit()
+
+        yield fillup.id
+
+        db.session.delete(fillup)
+        db.session.commit()
+
+
+@pytest.fixture
+def sample_fillup_2__serialized(sample_fillup_id_2, sample_vehicle_id_1):
+    yield {
+        "id": sample_fillup_id_2,
+        "timestamp": "2020-01-02T12:00:00",
+        "odometer_km": 1000,
+        "fuel_amt_l": 35,
+        "vehicle_id": sample_vehicle_id_1,
+        "vehicle": "2001 test-make1 test-model1",
     }
