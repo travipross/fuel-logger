a67f305065fe5ace6483bbf03a253b8b99decf06
Travis Prosser <travisprosser@gmail.com>
2023-10-09 15:03:38 -0300

    feat(api): Add route to post logs

--- tests/integration/api/test_api_logs.py
+++ tests/integration/api/test_api_logs.py
@@ -1,6 +1,7 @@
 import pytest
+
 from fuel_logger import db
-from fuel_logger.models import Vehicle, Fillup
+from fuel_logger.models import Fillup, Vehicle
 
 
 @pytest.fixture
@@ -59,3 +60,77 @@ def test_get_logs(test_client, basic_auth_header, test_vehicle_id, sample_fillup
     assert resp.status_code == 200
     assert len(resp.json) == 3
     assert set(sample_fillup_ids) == {f.get("id") for f in resp.json}
+
+
+def test_get_log_by_id(test_client, basic_auth_header, sample_fillup_ids):
+    resp = test_client.get(
+        f"/api/logs/{sample_fillup_ids[0]}",
+        headers=basic_auth_header,
+    )
+
+    assert resp.status_code == 200
+    assert resp.json.get("id") == sample_fillup_ids[0]
+    assert resp.json.get("fuel_amt_l") == 6
+    assert resp.json.get("odometer_km") == 100
+
+
+def test_get_log_by_id__non_existent(test_client, basic_auth_header):
+    resp = test_client.get(
+        f"/api/logs/1234",
+        headers=basic_auth_header,
+    )
+
+    assert resp.status_code == 404
+    assert resp.json.get("message") == "No log by that ID found."
+
+
+def test_pose_log__valid(app_fixture, test_client, basic_auth_header, test_vehicle_id):
+    resp = test_client.post(
+        f"/api/logs?vehicle_id={test_vehicle_id}",
+        headers=basic_auth_header,
+        json={
+            "odometer_km": 1234,
+            "fuel_amt_l": 69,
+        },
+    )
+
+    assert resp.status_code == 200
+    assert resp.json.get("odometer_km") == 1234
+    id = resp.json.get("id")
+    assert id is not None
+
+    # clean up
+    with app_fixture.app_context():
+        fillup = db.session.get(Fillup, id)
+        db.session.delete(fillup)
+        db.session.commit()
+
+
+def test_pose_log__bad_payload(test_client, basic_auth_header, test_vehicle_id):
+    resp = test_client.post(
+        f"/api/logs?vehicle_id={test_vehicle_id}",
+        headers=basic_auth_header,
+        json={
+            "odometer_km": 1234,
+            "fuel_amt_l": 69,
+            "banana": 1234,
+        },
+    )
+
+    assert resp.status_code == 500
+    assert resp.json.get("error") == "Internal Server Error"
+    assert "banana" in resp.json.get("message")
+
+
+def test_pose_log__no_vehicle(test_client, basic_auth_header):
+    resp = test_client.post(
+        f"/api/logs",
+        headers=basic_auth_header,
+        json={
+            "odometer_km": 1234,
+            "fuel_amt_l": 69,
+        },
+    )
+
+    assert resp.status_code == 400
+    assert resp.json.get("message") == "missing vehicle_id query parameter"
