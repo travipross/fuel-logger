34755b450188f3308dab9db5d1f37d813ae86383
Travis Prosser <travisprosser@gmail.com>
2023-10-09 03:09:24 -0300

    fix: Pin werkzeug to <3

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         "marshmallow-sqlalchemy",
         "pandas",
         "pyjwt",
+        "Werkzeug<3",
     ],
     entry_points={"console_scripts": ["fuel-logger = fuel_logger.fuel_logger:wsgi"]},
 )
